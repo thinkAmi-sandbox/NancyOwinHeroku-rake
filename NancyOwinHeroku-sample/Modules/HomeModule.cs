d13399bc89d96a219ee8d6f9e12be83f5005e3fd
thinkAmi <dev.thinkami+git@gmail.com>
2014-12-18 05:58:20 +0900

    first commit

---
+++ NancyOwinHeroku-sample/Modules/HomeModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+
+namespace NancyOwinHeroku_sample.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World";
+
+            Get["/json"] = _ =>
+            {
+                var ringo = new[]
+                {
+                    new { Id = 1, ItemName = "トキ"},
+                    new { Id = 2, ItemName = "秋映"}
+                };
+
+                return Response.AsJson(ringo);
+            };
+        }
+    }
+}
